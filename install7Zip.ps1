10a3c877ffe3687cbe8dae67c7e9f1add98ec8d8
Keshab Budhathoky <keshab.budhathoky@cbrands.com>
2025-08-07 11:42:39 -0400

    Add 7 zip installer script

---
+++ install7Zip.ps1
@@ -0,0 +1,19 @@
+write-host 'AIB Customization: Downloading 7-Zip installer'
+
+# Define the local path to save the installer
+$installerPath = 'C:\Windows\Temp\7z2301-x64.msi'
+
+
+# Define 7-Zip installer URL and file name
+$sevenzipInstallerURL = 'https://www.7-zip.org/a/7z2301-x64.msi'
+
+# Download 7-Zip installer
+Invoke-WebRequest -Uri $sevenzipInstallerURL -OutFile $installerPath
+
+write-host 'AIB Customization: 7-Zip installer downloaded'
+
+# Install 7-Zip silently
+Start-Process msiexec.exe -ArgumentList "/i `"$installerPath`" /quiet /norestart" -Wait
+
+Write-Host "AIB Customization: 7-Zip installed successfully"
+
